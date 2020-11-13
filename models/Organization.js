183f72ae7331114145603c0bb6fdcab032485d88
CallMeDKay <s3812649@rmit.edu.vn>
2020-11-14 06:28:01 +0700

    Define student model (+org model)

---
+++ models/Organization.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.json());
+
+mongoose.connect(
+	"mongodb+srv://hungthezorba:chelseaprovip123@mindxhackathon.x1ynp.mongodb.net/mindxhackathon?retryWrites=true&w=majority",
+	{ useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+var Organization = require("../schemas/OrganizationSchema");
+var OrganizationSchema = require("../schemas/OrganizationSchema").schema;
+
+router.get("/", (req, res) => {
+	Organization.find({}, (err, orgs) => {
+		if (err) handleError;
+		res.send(orgs);
+	});
+});
+
+router.get("/about", (req, res) => {
+	res.send({
+		msg: "This is API for organization",
+	});
+});
+
+router.post("/", (req, res) => {
+	Organization.find({}, (err, orgs) => {
+		Organization.findOne({ id: req.body.id }, async (err, org) => {
+			if (org) handleEror(err);
+			await Organization.create({
+				id: handleID(orgs),
+				name: req.body.name,
+				contact_phone: req.body.contact_phone,
+				address: req.body.address,
+			});
+			res.send({
+				msg: "Organization created successfully",
+			});
+		});
+	});
+});
+
+router.delete("/", async (req, res) => {
+	await Organization.deleteOne({ name: req.body.name });
+	res.send({
+		msg: "Organization deleted successfully",
+	});
+});
+
+function handleError(err) {
+	console.log(err);
+}
+
+function handleID(res) {
+	var length = res.length;
+	if (length == 0) {
+		return "1";
+	} else {
+		var last_item = res[length - 1].id;
+		last_item = parseInt(last_item, 10);
+		last_item += 1;
+		last_item = last_item.toString(10);
+		return last_item;
+	}
+}
+
+module.exports = router;
---
+++ schemas/UserSchema.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const OrganizationSchema = require('../schemas/OrganizationSchema').schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+    },
+    name: String,
+    email: String,
+    password: String,
+    dob: Date,
+    phone_number: String,
+    role: {
+        type: String,
+        required: true
+    },
+    organization: {
+        type: OrganizationSchema,
+    },
+    avatar_url: {
+        type: String,
+        required: false,
+        default: 'default.jpeg',
+    },
+})
+
+module.exports = mongoose.model('users', UserSchema);
