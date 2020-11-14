7a0e50e068b477d90fc6b3a4c495a450dc8db79d
hungnguyen <hungthezorba@gmail.com>
2020-11-14 16:54:05 +0700

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,28 @@ import NavigationBar from './components/navigation/NavigationBar.js';
 import LoginForm from './components/login/LoginForm';
 import SignUpForm from './components/signup/SignUpForm';
 import StudentHome from './pages/StudentHome';
+import Assignment from './pages/studentView/Assignment.js'
+import Course from './pages/studentView/Course'
 
 function App() {
   return (
     <BrowserRouter>
 			<div className="App">
-				<div className="wrapper">
-					<NavigationBar></NavigationBar>
+				<div className="box">
+					<div className="row header no-gutters">
+						<NavigationBar></NavigationBar>
+					</div>
+					<div className="row content no-gutters">
+						<div className="col-12">
 					<Switch>
-            <Route path="/signin" component={LoginForm}/>
-            <Route path='/signup' component={SignUpForm}/>
-            <Route path='/user' component={StudentHome}/>
+						<Route path="/signin" component={LoginForm}/>
+						<Route path='/signup' component={SignUpForm}/>
+						<Route path='/user' component={StudentHome}/>
+						<Route path='/assignment/:id' component={Assignment}/>
+						<Route path='/course' component={Course}/>
 					</Switch>
+					</div>
+					</div>
 				</div>
 			</div>
 		</BrowserRouter>
